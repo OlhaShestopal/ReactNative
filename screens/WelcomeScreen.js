148f0c80dd2c6ebb25eb6e914bdb30a0630ac456
Olha <oleg@MacBook-Air-Olha.local>
2021-04-07 10:15:26 +0300

    set and get data from click

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,62 +1,68 @@
 import React, { useEffect, useContext } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { StoreContext } from '../store';
-import * as types from '../store/actions';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
+// import * as types from '../store/actions';
 
 
 function WelcomeScreen() {
-    const {state, dispatch} = useContext(StoreContext)
-    const handleSetNewUrl = async(value) => {
-        try {
-          const newUrl = JSON.stringify(value)
-          await AsyncStorage.setItem('@storage_Link', newUrl)
-        } catch (e) {
-          alert(e)
-        }
-    }
-    const getData = async () => {
-      try {
-        return homeLink = await  AsyncStorage.getItem('@storage_Link')
-      } catch(e) {
-        return e
-      }
-    }
-    const handleGetUrl = (data) =>{
-    let urlLink;
-      let url = getData();
-      if (url) {
-        urlLink = data.home
-      } else {
-        urlLink = data.link
-      }
-      
-      dispatch({
-        type: types.GET_LINK,
-        paiload: urlLink
-      });
+const {state, dispatch} = useContext(StoreContext)
+const[localState, setLocalState] = useState({})
 
-      return urlLink;
+const setData = async(value) => {
+  try {
+    const newUrl = JSON.stringify(value)
+    await AsyncStorage.setItem('@data', newUrl)
+  } catch (e) {
+    console.log(e)
+  }
+}
 
+const getData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key)
+    if(value !== null) {
+      console.log(`OK get data:${value} `)
+    } else{
+      console.log('get null')
     }
-    const handleCloseScreen = () => {
-      dispatch({
-        type: types.CLOSE_WELCOME,
-        }
-      )
-    }
-    useEffect(() => {
-      fetch('https://efs5i1ube5.execute-api.eu-central-1.amazonaws.com/prod')
-        .then((response) => response.json())
-        .then((json) => handleGetUrl(json))
-        .then((value)=> handleSetNewUrl(value))
-        .then(() => handleCloseScreen())
-        
-    }, []);
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+// function handleGetFirsState (localState){
+// dispatch({
+//   type: types.GET_URL,
+//   paiload: localState
+// })
+
+// }
+ useEffect( () => {
+    fetch('https://co98qyg3i6.execute-api.eu-central-1.amazonaws.com/real?app=com.blumine.himalaya&uuid=test&uid=test&a=4&o=1221&s3=devtest')
+    .then((response) => response.json())
+    .then((json) => setLocalState(json))
+    .then(() => setData(localState))
+    .then(() => console.log(localState))
+    // .then (() => handleGetFirsState())
+    .catch((e)=>console.log(e))
+  
     
+}, []);
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Loading ...</Text>
+        <Button 
+          onPress={()=>setData(localState)}
+          title= 'Set'/>
+          <Button
+          title = 'GET'
+          onPress={() =>getData('@data')}
+          />
+          <Button 
+          title = 'Fetch'
+          onPress = {() => console.log(localState)}/>
       </View>
     );
 }
--- store/state.js
+++ store/state.js
@@ -1,7 +1,8 @@
 const initialState = {
     isLoading: true,
     isFirstLoading: true,
-    link: null,
+    link: {},
+    
 }
 
 export{
